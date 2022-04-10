7a0afc1235e70b5b8422ae66389e4007014b9a38
Frank <33519926+Conengmo@users.noreply.github.com>
2022-04-10 19:05:42 +0200

    add new (fixed) layer on click

--- dndmap/urls.py
+++ dndmap/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("map/create/", views_map.CreateView.as_view(), name="create_map"),
     path("map/<int:pk>/", views_map.get_map, name="get_map"),
     path("map/<int:pk>/components", views_components.get_components, name='get_components'),
+    path("map/<int:pk>/components/add", views_components.add_new_marker, name='add_new_marker'),
 ]
--- dndmap/views/components.py
+++ dndmap/views/components.py
@@ -1,8 +1,10 @@
+import json
+
 from django.contrib.auth.decorators import login_required
-from django.http import HttpRequest, JsonResponse
+from django.http import HttpRequest, JsonResponse, HttpResponse
 
 from dndmap.decorators import get_map_obj
-from dndmap.models import Map
+from dndmap.models import Map, Marker
 
 
 @get_map_obj
@@ -12,3 +14,14 @@ def get_components(request: HttpRequest, map_obj: Map):
         [layer.to_dict() for layer in map_obj.layer_set.all()],
         safe=False,
     )
+
+
+@get_map_obj
+@login_required
+def add_new_marker(request: HttpRequest, map_obj: Map):
+    data = json.loads(request.body)
+    Marker.objects.create(
+        layer=map_obj.layer_set.first(),
+        **data,
+    )
+    return HttpResponse()
